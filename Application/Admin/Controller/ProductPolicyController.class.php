6eca5f8631375b7a9040d5babc2e07a928779359
Jianjia Zheng/郑建家/BPJianjia.zhengIT <jianjia.zheng@phicomm.com>
2018-07-12 18:33:49 +0800

    策略c jan

---
+++ Application/Admin/Controller/ProductPolicyController.class.php
@@ -0,0 +1,20 @@
+<?php
+namespace Admin\Controller;
+use Admin\Model\SystemKeysModel;
+use Admin\Model\BaseModel;
+
+/**
+ * 策略管理
+ * @author jan
+ *
+ */
+class ProductPolicyController extends BaseController
+{
+
+    //待分配K码列表
+    public function index() {
+
+    }
+
+
+}
