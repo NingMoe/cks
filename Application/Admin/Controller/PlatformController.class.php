c3b16072b7236d5f95aa2bcc025f28684704cf4a
Daoxin Wang/王道鑫/PD/SMART HOME <daoxin.wang@phicomm.com>
2018-07-23 14:41:36 +0800

    平台营业时间业务

---
+++ Application/Admin/Controller/PlatformController.class.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Admin\Controller;
+use Admin\Model\AccountModel;
+use Admin\Model\BaseModel;
+
+/**
+ * 用于用户的登录验证控制器类
+ * 
+ * @author jan
+ *
+ */
+class PlatformController extends BaseController
+{
+    /**
+     * 平台列表
+     */
+    public function index()
+    {
+       $this->assign('platforms', M('platform')->select());
+       $this->display();
+    }
+
+    public function edit(){
+        $id = I('get.platformId');
+        $this->assign('platform', M('platform')->where(['id' => $id]))->find();
+        $this->display();
+    }
+
+    public function update()
+    {
+
+    }
+
+}
