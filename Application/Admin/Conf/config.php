e9b22fd476894d0f29b13e6a4d6d5470e030390b
Jianjia Zheng/郑建家/BPJianjia.zhengIT <jianjia.zheng@phicomm.com>
2018-07-24 09:59:13 +0800

    公告管理

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -2,16 +2,13 @@
 return array(
 	//'配置项'=>'配置值'
 
-    'articleType' => ['新闻动态', '公告'],
-
     'version' => 'v1.0.0.1',
 
     //上传路径
     'ADMIN_PATH'=>[
-        'SETEMLINK' => 'sicon',
-        'PAGEBANNER' => 'banner',
-        'HOLIDAY' => 'holiday',
-        'ARTICLE' => 'article',
         'KINDEDITOR' => 'kindfiles',
-    ]
+    ],
+
+    //uploadUrl
+    'uploadCksApiUrl' => 'https://k-test.phicomm.com/cks_api/index.php/API/Base/upload',
 );
